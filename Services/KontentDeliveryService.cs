93dcbccafc1b251d672c06f160a05c5a31922b16
Brian McKeiver <bmckeiver@bizstream.com>
2020-01-01 11:04:05 -0500

    Integrating Kentico Kontent Delivery SDK and making the first TeamService work.

---
+++ Services/KontentDeliveryService.cs
@@ -0,0 +1,33 @@
+﻿using Kentico.Kontent.Delivery;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+
+namespace RowlingApp.Services
+{
+    public class KontentDeliveryService
+    {        
+        private IConfiguration _configuration;
+        private HttpClient _httpClient;
+        private ITypeProvider _typeProvider;
+        private IDeliveryClient _deliveryClient;
+
+        public KontentDeliveryService(IConfiguration configuration, HttpClient httpClient, ITypeProvider typeProvider)
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+            _typeProvider = typeProvider;
+        }
+
+        public IDeliveryClient GetDeliveryClient()
+        {
+            if (_deliveryClient == null)
+            {
+                var projectId = _configuration.GetValue<string>("DeliveryOptions:ProjectId");
+                _deliveryClient = DeliveryClientBuilder.WithProjectId(projectId).WithHttpClient(_httpClient).WithTypeProvider(_typeProvider).Build();
+            }
+
+            return _deliveryClient;
+        }
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RowlingApp.Data;
+using RowlingApp.Services;
+using Kentico.Kontent.Delivery;
+using RowlingApp.Models;
 
 namespace RowlingApp
 {
@@ -26,9 +29,14 @@ namespace RowlingApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddHttpClient();
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ITypeProvider, CustomTypeProvider>();
+            services.AddHttpClient<KontentDeliveryService>();
+            services.AddSingleton<TeamService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
