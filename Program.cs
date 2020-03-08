3fbc2da95828d4c8afd36f8195f9baedb89f512b
Brian McKeiver <bmckeiver@bizstream.com>
2020-03-08 16:18:04 -0400

    Added in environment variable detection to match to the lables of the feature flags

--- Program.cs
+++ Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System;
 
 namespace RowlingApp
 {
@@ -19,34 +13,51 @@ namespace RowlingApp
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            //Default implementation for Blazor server for CreateDefaultBuilder
-            //Host.CreateDefaultBuilder(args)
-            //    .ConfigureWebHostDefaults(webBuilder =>
-            //    {
-            //        webBuilder.UseStartup<Startup>();
-            //    });
+         /* 1. Default out of the box implementation for Blazor server for CreateDefaultBuilder
+          */
+         //Host.CreateDefaultBuilder(args)
+         //    .ConfigureWebHostDefaults(webBuilder =>
+         //    {
+         //        webBuilder.UseStartup<Startup>();
+         //    });
 
-            //Adding in Azure App Configuration and Feature Flags to our Builder
-            //ensure your app.
-            Host.CreateDefaultBuilder(args)
+
+         /* 2. Adding in Azure App Configuration and Feature Flags to our Builder
+          *     ensure your app. Uses default options (caches every 30 seconds)
+          *     Make sure you have ConnectionStrings:AppConfig in your user's secrets.json and in Azure App Service AppSettings 
+          */
+         //Host.CreateDefaultBuilder(args)
+         //   .ConfigureWebHostDefaults(webBuilder =>
+         //    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+         //    {
+         //       var settings = config.Build();
+         //       config.AddAzureAppConfiguration(options => {
+         //           options.Connect(settings["ConnectionStrings:AppConfig"])
+         //                  .UseFeatureFlags();
+         //       });
+         //    })
+         //    .UseStartup<Startup>());
+
+
+         /* 3. Same as above but with label filter and custom cache expiration time (set to every 15 seconds).
+          *     The label must matche the running environment's environment variable 
+          *     (same as env.IsDevelopment() or env.IsProduction())
+          */
+         Host.CreateDefaultBuilder(args)
                .ConfigureWebHostDefaults(webBuilder =>
                 webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                   var settings = config.Build();
-                   config.AddAzureAppConfiguration(options => {
-                       options.Connect(settings["ConnectionStrings:AppConfig"])
-                              .UseFeatureFlags(); //with default options (caches every 30 seconds)
-                   });
-
-
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options =>
+                    {
+                        options.Connect(settings["ConnectionStrings:AppConfig"])
+                            .UseFeatureFlags(opt => {
+                                opt.CacheExpirationTime = TimeSpan.FromSeconds(15); 
+                                opt.Label = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?.ToLowerInvariant();
+                            }); 
+                    });
                 })
                 .UseStartup<Startup>());
 
-
-            //  .UseFeatureFlags(opt => {
-            //      opt.CacheExpirationTime = TimeSpan.FromSeconds(5); // Set cache expiry to 5 seconds
-            //  });
-
-
-        }
     }
+}
